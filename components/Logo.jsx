18b2c8c60769a71a79618aea8d51195908d2a04b
Md Jahid Hasan <jahidhasan658560@gmail.com>
2025-07-01 23:31:43 +0600

    Refactor code structure for improved readability and maintainability

--- components/Logo.jsx
+++ components/Logo.jsx
@@ -1,9 +1,19 @@
-import React from 'react';
+import Link from "next/link";
+import Image from "next/image";
 
 const Logo = () => {
     return (
-        <div>
-            <h1>Logo</h1>
+        <div className="lg:flex">
+            <Link href="/">
+                <Image
+                    className="h-6 w-auto"
+                    src="/logo.svg"
+                    alt="Logo"
+                    width={100}
+                    height={24}
+                    priority
+                />
+            </Link>
         </div>
     );
 };
--- lib/documents.ts
+++ lib/documents.ts
@@ -1,37 +1,55 @@
-import fs from 'fs';
-import path from 'path';
-import matter from 'gray-matter';
-import { remark } from 'remark';
-import  html   from 'remark-html';
+import fs from "fs";
+import path from "path";
+import matter from "gray-matter";
 
-const postsDirectory = path.join(process.cwd(), 'docs');
+import { remark } from "remark";
+import html from "remark-html";
 
+const postsDirectory = path.join(process.cwd(), "docs");
 
-export function getDocuments(){
+export function getDocuments() {
     console.log(postsDirectory);
     const fileNames = fs.readdirSync(postsDirectory);
 
-    const allDocuments = fileNames.map(fileNames =>{
-        const id = fileNames.replace(".md", "")
+    const allDocumnets = fileNames.map((fileName) => {
+        const id = fileName.replace(".md", "");
 
-        const fullPath = path.join(postsDirectory, fileNames);
+        const fullPath = path.join(postsDirectory, fileName);
 
-        const fileContents = fs.readFileSync(fullPath, "utf-8")
+        const fileContents = fs.readFileSync(fullPath, "utf8");
 
         const matterResult = matter(fileContents);
 
-
-        return{
+        return {
             id,
-            ...matterResult.data
-        }
+            ...matterResult.data,
+        };
     });
-    return allDocuments.sort((a, b) => {
-        if (a.order <b.order){
-            return -1
+
+    return allDocumnets.sort((a, b) => {
+        if (a.order < b.order) {
+            return -1;
+        }
+        if (a.order > b.order) {
+            return 1;
         }
         return 0;
-    })
+    });
+}
 
+export async function getDocumentContent(id) {
+    const fullPath = path.join(postsDirectory, `${id}.md`);
+    const fileContents = fs.readFileSync(fullPath, "utf8");
 
+    const matterResult = matter(fileContents);
+
+    const processedContent = await remark().use(html).process(matterResult.content);
+
+    const contentHtml = processedContent.toString();
+
+    return {
+        id,
+        contentHtml,
+        ...matterResult.data,
+    }
 }
